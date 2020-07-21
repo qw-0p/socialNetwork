cf5974cc67011ae7ce8844db65b43e3db9d3308c
Aleksandr Sarkisov <quopiks@gmail.com>
2020-07-21 21:30:03 +0300

    added classes in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,19 @@ export const Navbar = () => {
     return (
         <nav className={s.nav}>
             <ul>
-                <li className="item">
+                <li className={s.item}>
                     <a href="#">Profile</a>
                 </li>
-                <li className="item">
+                <li className={s.item}>
                     <a href="#">Messages</a>
                 </li>
-                <li className="item">
+                <li className={s.item}>
                     <a href="#">News</a>
                 </li>
-                <li className="item">
+                <li className={s.item}>
                     <a href="#">Music</a>
                 </li>
-                <li className="item">
+                <li className={s.item}>
                     <a href="#">Settings</a>
                 </li>
             </ul>
