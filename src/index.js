c828462880c1dd7093fe60a48463465cc41ff5a2
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-04 20:22:17 +0300

    remove render.js, removed cyclic dependency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-import {rerenderEntireTree} from './render';
-import state from './redux/state';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import state, {addPost, updateNewPostText, subscribe} from './redux/state';
 
+let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App
+                state={state}
+                addPost={addPost}
+                updateNewPostText={updateNewPostText}
+            />
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+};
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
