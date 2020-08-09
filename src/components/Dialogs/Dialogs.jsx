f9533a5d43fffa7999443f12bd5f1f98af08564e
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-09 15:58:46 +0300

    added stateCopy for dialogs reducer and added key for Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,10 +6,10 @@ import DialogItem from './DialogItem/DialogItem';
 function Dialogs(props) {
     let state = props.dialogsPage;
     let dialogsElements = state.dialogs.map((d) => (
-        <DialogItem name={d.name} id={d.id} />
+        <DialogItem name={d.name} key={d.id} id={d.id} />
     ));
     let messagesElements = state.messages.map((m) => (
-        <Message message={m.message} />
+        <Message message={m.message} key={m.id} />
     ));
     let newMessageBody = state.newMessageBody;
 
--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -13,11 +13,11 @@ let mapStateToProps = (state) => {
 };
 let mapDispatchToProps = (dispatch) => {
     return {
-        updateNewMessageBody: () => {
-            dispatch(sendMessageCreator());
-        },
-        sendMessage: (body) => {
+        updateNewMessageBody: (body) => {
             dispatch(updateNewMessageBodyCreator(body));
+        },
+        sendMessage: () => {
+            dispatch(sendMessageCreator());
         },
     };
 };
