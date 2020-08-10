31cf9de8919147e9f06eb4374528e78299a5ba86
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-10 13:28:30 +0300

    added component Users

--- src/App.js
+++ src/App.js
@@ -4,25 +4,22 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import Users from './components/Users/Users';
 
 const App = (props) => {
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header />
-                <Navbar />
-                <div className="app-wrapper-content">
-                    <Route
-                        path="/dialogs"
-                        render={() => <DialogsContainer />}
-                    />
-                    <Route path="/profile" render={() => <Profile />} />
-                </div>
+        <div className="app-wrapper">
+            <Header />
+            <Navbar />
+            <div className="app-wrapper-content">
+                <Route path="/dialogs" render={() => <DialogsContainer />} />
+                <Route path="/profile" render={() => <Profile />} />
+                <Route path="/users" render={() => <Users />} />
             </div>
-        </BrowserRouter>
+        </div>
     );
 };
 
