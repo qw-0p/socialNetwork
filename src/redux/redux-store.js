19dde50b9536798d550657ce34b7afa3e75e6f2c
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-10 14:12:59 +0300

    added and set up component UsersContainer

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,6 +1,7 @@
 import profileReducer from './profileReducer';
 import dialogsReducer from './dialogsReducer';
 import sidebarReducer from './sidebarReducer';
+import usersReducer from './usersReducer';
 
 const {createStore, combineReducers} = require('redux');
 
@@ -8,6 +9,7 @@ let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
+    usersPage: usersReducer,
 });
 
 let store = createStore(reducers);
