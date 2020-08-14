748709d578659c0b269cbab27be87563752ad802
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-14 16:57:37 +0300

    moved class UsersAPIComponent to UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -8,6 +8,46 @@ import {
     setCurrentPageAC,
     setTotalUsersCountAC,
 } from '../../redux/usersReducer';
+import * as axios from 'axios';
+import Users from './Users';
+
+class UsersAPIComponent extends React.Component {
+    componentDidMount() {
+        axios
+            .get(
+                `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+            )
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNmuber) => {
+        this.props.setCurrentPage(pageNmuber);
+        axios
+            .get(
+                `https://social-network.samuraijs.com/api/1.0/users?page=${pageNmuber}&count=${this.props.pageSize}`
+            )
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+            });
+    };
+
+    render() {
+        return (
+            <Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                unfollow={this.props.unfollow}
+                follow={this.props.follow}
+            />
+        );
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
