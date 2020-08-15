f879141a4492dd118d021962f284dcf6618f2a8b
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-15 12:53:44 +0300

    set up ProfileContainer component

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,5 +1,6 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET_USER_PROFILE';
 
 let initialState = {
     posts: [
@@ -9,6 +10,7 @@ let initialState = {
         {id: 4, message: 'Good', likesCount: 14},
     ],
     newPostText: '',
+    profile: null,
 };
 
 const profileReducer = (state = initialState, action) => {
@@ -31,6 +33,12 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: action.newText,
             };
         }
+        case SET_USER_PROFILE: {
+            return {
+                ...state,
+                profile: action.profile,
+            };
+        }
         default:
             return state;
     }
@@ -38,6 +46,10 @@ const profileReducer = (state = initialState, action) => {
 
 export const addPostActionCreator = () => ({
     type: ADD_POST,
+});
+export const setUserProfile = (profile) => ({
+    type: SET_USER_PROFILE,
+    profile,
 });
 
 export const updateNewPostTextActionCreator = (text) => ({
