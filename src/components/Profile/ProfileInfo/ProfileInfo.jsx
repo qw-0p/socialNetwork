8cf88b29ea7246158f2cf4d3b3c4641c9f752e70
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-15 13:09:23 +0300

    set up ProfileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import s from './ProfileInfo.module.scss';
+import Preloader from '../../common/Preloader/Preloader';
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if (!props.profile) {
+        return <Preloader />;
+    }
+
     return (
         <div>
             <div>
@@ -11,7 +16,15 @@ const ProfileInfo = () => {
                     className={s.titleImg}
                 />
             </div>
-            <div className={s.descriptionBlock}>ava + desc</div>
+            <div className={s.descriptionBlock}>
+                <img src={props.profile.photos.large} />
+                <h2>{props.profile.fullName}</h2>
+                <p className={s.description}>
+                    {props.profile.lookingForAJob
+                        ? props.profile.lookingForAJobDescription
+                        : 'Не ищу работу'}
+                </p>
+            </div>
         </div>
     );
 };
