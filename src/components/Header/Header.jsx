9be6f170d69edd1de3f625502e721440e5679372
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-16 02:50:38 +0300

    set up auth for Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import s from './Header.module.scss';
 import {NavLink} from 'react-router-dom';
 
-export default function Header() {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img
@@ -10,8 +10,13 @@ export default function Header() {
                 alt=""
             />
             <div className={s.loginBlock}>
-                <NavLink to={'/login'}>Login</NavLink>
+                {props.isAuth ? (
+                    props.login
+                ) : (
+                    <NavLink to={'/login'}>Login</NavLink>
+                )}
             </div>
         </header>
     );
-}
+};
+export default Header;
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -4,7 +4,7 @@ let initialState = {
     id: null,
     email: null,
     login: null,
-    isFetching: false,
+    isAuth: false,
 };
 
 const authReducer = (state = initialState, action) => {
@@ -13,6 +13,7 @@ const authReducer = (state = initialState, action) => {
             return {
                 ...state,
                 ...action.data,
+                isAuth: true,
             };
 
         default:
@@ -20,7 +21,7 @@ const authReducer = (state = initialState, action) => {
     }
 };
 
-export const setUserData = (id, email, login) => ({
+export const setAuthUserData = (id, email, login) => ({
     type: SET_USER_DATA,
     data: {id, email, login},
 });
