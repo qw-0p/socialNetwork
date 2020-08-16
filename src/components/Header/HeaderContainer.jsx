14e37ea1f4704873b82ad72759c770780f9054ca
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-16 03:01:51 +0300

    fix bug with HeaderContainer

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from './Header';
 import * as axios from 'axios';
-import setAuthUserData from '../../redux/authReducer';
 import {connect} from 'react-redux';
+import {setAuthUserData} from '../../redux/authReducer';
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
@@ -13,15 +13,15 @@ class HeaderContainer extends React.Component {
             .then((response) => {
                 if (response.data.resultCode === 0) {
                     let {id, login, email} = response.data.data;
-                    this.props.setAuthUserData(response.data.data);
+                    this.props.setAuthUserData(id, email, login);
                 }
             });
     }
+
     render() {
         return <Header {...this.props} />;
     }
 }
-
 const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
