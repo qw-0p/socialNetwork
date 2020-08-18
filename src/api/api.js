8b4a9d82570d678d69ed2507251db6c41f805ef8
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-18 20:38:20 +0300

    refactored api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,23 @@
 import * as axios from 'axios';
 
 const instance = axios.create({
-    withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-    headers: {
-        'API-KEY': 'fe890eae-1cd2-4a43-a8c1-baca1b285e21',
-    },
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': 'fe890eae-1cd2-4a43-a8c1-baca1b285e21',
+  },
 });
 
 export const usersAPI = {
-    getUsers(currentPage = 1, pageSize = 10) {
-        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => {
-            return response.data;
-        });
-    },
-    follow(userId) {
-        return instance.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`);
-    },
-    unfollow(userId) {
-        return instance.delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`);
-    },
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => {
+      return response.data;
+    });
+  },
+  follow(userId) {
+    return instance.post(`follow/${userId}`);
+  },
+  unfollow(userId) {
+    return instance.delete(`follow/${userId}`);
+  },
 };
