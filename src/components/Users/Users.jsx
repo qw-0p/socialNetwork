54f340934ae9b761b1a111a8d7150fbe6e9e5a4c
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-18 20:35:25 +0300

    removed api from Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,83 +6,71 @@ import * as axios from 'axios';
 import {usersAPI} from '../../api/api';
 
 let Users = (props) => {
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
 
-    return (
-        <div>
+  return (
+    <div>
+      <div>
+        {pages.map((p) => {
+          return (
+            <span
+              className={props.currentPage === p && styles.selectedPage}
+              onClick={(e) => {
+                props.onPageChanged(p);
+              }}
+              key={p.id}>
+              {p}
+            </span>
+          );
+        })}
+      </div>
+      {props.users.map((u) => (
+        <div key={u.id}>
+          <span>
             <div>
-                {pages.map((p) => {
-                    return (
-                        <span
-                            className={props.currentPage === p && styles.selectedPage}
-                            onClick={(e) => {
-                                props.onPageChanged(p);
-                            }}
-                            key={p.id}>
-                            {p}
-                        </span>
-                    );
-                })}
+              <NavLink to={'/profile/' + u.id}>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt='' />
+              </NavLink>
             </div>
-            {props.users.map((u) => (
-                <div key={u.id}>
-                    <span>
-                        <div>
-                            <NavLink to={'/profile/' + u.id}>
-                                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt='' />
-                            </NavLink>
-                        </div>
-                        <div>
-                            {u.followed ? (
-                                <button
-                                    disabled={props.followingInProgress.some((id) => id === u.id)}
-                                    onClick={() => {
-                                        props.toggleFollowingProgress(true, u.id);
-                                        usersAPI.unfollow(u.id).then((response) => {
-                                            if (response.data.resultCode === 0) {
-                                                props.unfollow(u.id);
-                                            }
-                                            props.toggleFollowingProgress(false, u.id);
-                                        });
-                                    }}>
-                                    Unfollow
-                                </button>
-                            ) : (
-                                <button
-                                    disabled={props.followingInProgress.some((id) => id === u.id)}
-                                    onClick={() => {
-                                        props.toggleFollowingProgress(true, u.id);
-                                        usersAPI.follow(u.id).then((response) => {
-                                            if (response.data.resultCode === 0) {
-                                                props.follow(u.id);
-                                            }
-                                            props.toggleFollowingProgress(false, u.id);
-                                        });
-                                    }}>
-                                    Follow
-                                </button>
-                            )}
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>{u.name}</div>
-                            <div>{u.status}</div>
-                        </span>
-                        <span>
-                            <div>{'u.location.country'}</div>
-                            <div>{'u.location.city'}</div>
-                        </span>
-                    </span>
-                </div>
-            ))}
+            <div>
+              {u.followed ? (
+                <button
+                  disabled={props.followingInProgress.some((id) => id === u.id)}
+                  onClick={() => {
+                    props.unfollow(u.id);
+                  }}>
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  disabled={props.followingInProgress.some((id) => id === u.id)}
+                  onClick={() => {
+                    props.follow(u.id);
+                  }}>
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </span>
+            <span>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
+            </span>
+          </span>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Users;
