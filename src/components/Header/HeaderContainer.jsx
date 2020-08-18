5198073a83a234ff3a7a3640545d9aca4e08e787
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-18 21:20:58 +0300

    created thunk for header

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import {connect} from 'react-redux';
-import {setAuthUserData} from '../../redux/authReducer';
+import {getAuthUserData} from '../../redux/authReducer';
 import {authAPI} from '../../api/api';
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    authAPI.me().then((response) => {
-      if (response.data.resultCode === 0) {
-        let {id, login, email} = response.data.data;
-        this.props.setAuthUserData(id, email, login);
-      }
-    });
+    this.props.getAuthUserData();
   }
 
   render() {
@@ -23,4 +18,4 @@ const mapStateToProps = (state) => ({
   login: state.auth.login,
 });
 
-export default connect(mapStateToProps, {setAuthUserData})(HeaderContainer);
+export default connect(mapStateToProps, {getAuthUserData})(HeaderContainer);
--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -1,29 +1,40 @@
+import {authAPI} from '../api/api';
+
 const SET_USER_DATA = 'SET_USER_DATA';
 
 let initialState = {
-    id: null,
-    email: null,
-    login: null,
-    isAuth: false,
+  id: null,
+  email: null,
+  login: null,
+  isAuth: false,
 };
 
 const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case SET_USER_DATA:
-            return {
-                ...state,
-                ...action.data,
-                isAuth: true,
-            };
+  switch (action.type) {
+    case SET_USER_DATA:
+      return {
+        ...state,
+        ...action.data,
+        isAuth: true,
+      };
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
 export const setAuthUserData = (id, email, login) => ({
-    type: SET_USER_DATA,
-    data: {id, email, login},
+  type: SET_USER_DATA,
+  data: {id, email, login},
 });
+
+export const getAuthUserData = () => (dispatch) => {
+  authAPI.me().then((response) => {
+    if (response.data.resultCode === 0) {
+      let {id, login, email} = response.data.data;
+      dispatch(setAuthUserData(id, email, login));
+    }
+  });
+};
 
 export default authReducer;
