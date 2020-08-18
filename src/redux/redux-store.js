17199ebda3c1fe6fcb3ca6083e9694c4d610a93b
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-18 19:49:54 +0300

    install redux-thunk

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,7 +4,7 @@ import sidebarReducer from './sidebarReducer';
 import usersReducer from './usersReducer';
 import authReducer from './authReducer';
 
-const {createStore, combineReducers} = require('redux');
+const {createStore, combineReducers, applyMiddleware} = require('redux');
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -14,6 +14,6 @@ let reducers = combineReducers({
     auth: authReducer,
 });
 
-let store = createStore(reducers);
+let store = createStore(reducers, applyMiddleware());
 
 export default store;
