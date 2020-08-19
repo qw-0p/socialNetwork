d4fe27203ebc7c605a7dc0ba80afd533916f7cf9
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-19 17:52:56 +0300

    refactoring AuthRedirectComponent, created withAuthRedirect

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,6 +3,7 @@ import Profile from './Profile';
 import {connect} from 'react-redux';
 import {getUserProfile} from '../../redux/profileReducer';
 import {withRouter, Redirect} from 'react-router-dom';
+import {withAuthRedirect} from '../../hoc/withAuthRedirect';
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
@@ -14,12 +15,11 @@ class ProfileContainer extends React.Component {
   }
 
   render() {
-    if (!this.props.isAuth) {
-      return <Redirect to={'/login'} />;
-    }
     return <Profile {...this.props} profile={this.props.profile} />;
   }
 }
+
+let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
 
 let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
