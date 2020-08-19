fe1627dc1863a394e1bbf93e64df57d8dfc25458
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-19 16:48:20 +0300

    deleted useless import

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import {connect} from 'react-redux';
 import {getAuthUserData} from '../../redux/authReducer';
-import {authAPI} from '../../api/api';
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
