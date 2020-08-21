dce3eadf4e2075458a2ad2737b618550732c0550
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-21 11:30:39 +0300

    set up PrfoileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
-const ProfileStatus = (props) => {
-    return (
-        <div>
+class ProfileStatus extends React.Component {
+    state = {
+        editMode: false
+    };
+    activateEditMode() {
+        this.setState({
+            editMode: true
+        });
+    }
+    deactivateEditMode() {
+        this.setState({
+            editMode: false
+        });
+    }
+    render() {
+        return (
             <div>
-                <span>{props.status}</span>
+                {!this.state.editMode && (
+                    <div>
+                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                    </div>
+                )}
+                {this.state.editMode && (
+                    <div>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status} />
+                    </div>
+                )}
             </div>
-            <div>
-                <input value={props.status} />
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default ProfileStatus;
