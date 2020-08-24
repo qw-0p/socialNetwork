446c66a56dfd95236ff48e17dab4c9f30245ed32
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-24 18:27:21 +0300

    removed useless imports

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {updateNewMessageBodyCreator, sendMessageCreator} from '../../redux/dialogsReducer';
 import Dialogs from './Dialogs';
 import {connect} from 'react-redux';
@@ -6,19 +5,19 @@ import {withAuthRedirect} from '../../hoc/withAuthRedirect';
 import {compose} from 'redux';
 
 let mapStateToProps = (state) => {
-  return {
-    dialogsPage: state.dialogsPage,
-  };
+    return {
+        dialogsPage: state.dialogsPage
+    };
 };
 let mapDispatchToProps = (dispatch) => {
-  return {
-    updateNewMessageBody: (body) => {
-      dispatch(updateNewMessageBodyCreator(body));
-    },
-    sendMessage: () => {
-      dispatch(sendMessageCreator());
-    },
-  };
+    return {
+        updateNewMessageBody: (body) => {
+            dispatch(updateNewMessageBodyCreator(body));
+        },
+        sendMessage: () => {
+            dispatch(sendMessageCreator());
+        }
+    };
 };
 
 export default compose(connect(mapStateToProps, mapDispatchToProps), withAuthRedirect)(Dialogs);
