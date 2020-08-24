5787e8d8bbb2e4f1c9950be47314e3a7d84f74a6
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-24 18:39:02 +0300

    created LoginForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Login = (props) => {
-  return <h1>Login</h1>;
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input type='text' placeholder={'Login'} />
+            </div>
+            <div>
+                <input type='password' placeholder={'Password'} />
+            </div>
+            <div>
+                <input type={'checkbox'} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    );
 };
-
+const Login = (props) => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm />
+        </div>
+    );
+};
 export default Login;
