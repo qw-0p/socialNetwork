d51290343e01afd174cedd7b820386ab3da5590d
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-27 14:40:40 +0300

    added Redirect after login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,6 +4,7 @@ import {Input} from '../common/FormsControls/FormsControls';
 import {required} from '../../utils/validators/validators';
 import {connect} from 'react-redux';
 import {login} from '../../redux/authReducer';
+import {Redirect} from 'react-router-dom';
 
 const LoginForm = (props) => {
     return (
@@ -33,6 +34,10 @@ const Login = (props) => {
         props.login(formData.email, formData.password, formData.rememberMe);
     };
 
+    if (props.isAuth) {
+        return <Redirect to={'/profile'} />;
+    }
+
     return (
         <div>
             <h1>Login</h1>
@@ -41,4 +46,8 @@ const Login = (props) => {
     );
 };
 
-export default connect(null, {login})(Login);
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+});
+
+export default connect(mapStateToProps, {login})(Login);
