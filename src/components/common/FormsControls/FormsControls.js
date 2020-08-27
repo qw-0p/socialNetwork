33d559c1bbbc2e796e672df1f72ebebcb8a909a5
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-27 13:04:58 +0300

    refactoring

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import s from './FormsControls.module.scss';
 
-export const Textarea = ({input, meta, ...props}) => {
+const FormControl = ({input, meta, ...props}) => {
     const hasError = meta.touched && meta.error;
     return (
         <div className={s.formControl + ' ' + (hasError ? s.error : '')}>
-            <div>
-                <textarea {...input} {...props} />
-            </div>
+            <div>{props.children}</div>
             {hasError && <span>{meta.error}</span>}
         </div>
     );
 };
-export const Input = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error;
+
+export const Textarea = (props) => {
+    const {input, meta, child, ...restProps} = props;
     return (
-        <div className={s.formControl + ' ' + (hasError ? s.error : '')}>
-            <div>
-                <input {...input} {...props} />
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
+        <FormControl {...props}>
+            <textarea {...input} {...restProps} />
+        </FormControl>
+    );
+};
+export const Input = (props) => {
+    const {input, meta, child, ...restProps} = props;
+    return (
+        <FormControl {...props}>
+            <input {...input} {...restProps} />
+        </FormControl>
     );
 };
