a12de3bbb81abbd225baa5a9b175d8811e3c5213
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-27 14:49:28 +0300

    added logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import s from './Header.module.scss';
 import {NavLink} from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
 
 const Header = (props) => {
-  return (
-    <header className={s.header}>
-      <img src='https://c7.hotpng.com/preview/266/92/934/iphone-computer-icons-apple-messages-whatsapp-whatsapp.jpg' alt='' />
-      <div className={s.loginBlock}>{props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}</div>
-    </header>
-  );
+    return (
+        <header className={s.header}>
+            <img src={logo} alt='' />
+            <div className={s.loginBlock}>
+                {props.isAuth ? (
+                    <div>
+                        {props.login} - <button onClick={props.logout}>Log out</button>
+                    </div>
+                ) : (
+                    <NavLink to={'/login'}>Login</NavLink>
+                )}
+            </div>
+        </header>
+    );
 };
 export default Header;
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Header from './Header';
 import {connect} from 'react-redux';
-import {getAuthUserData} from '../../redux/authReducer';
+import {getAuthUserData, logout} from '../../redux/authReducer';
 
 class HeaderContainer extends React.Component {
-  componentDidMount() {
-    this.props.getAuthUserData();
-  }
+    componentDidMount() {
+        this.props.getAuthUserData();
+    }
 
-  render() {
-    return <Header {...this.props} />;
-  }
+    render() {
+        return <Header {...this.props} />;
+    }
 }
 const mapStateToProps = (state) => ({
-  isAuth: state.auth.isAuth,
-  login: state.auth.login,
+    isAuth: state.auth.isAuth,
+    login: state.auth.login
 });
 
-export default connect(mapStateToProps, {getAuthUserData})(HeaderContainer);
+export default connect(mapStateToProps, {getAuthUserData, logout})(HeaderContainer);
