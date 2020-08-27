c3ac3bf74ff556027b280de36c1d81e3975c5a83
Aleksandr Sarkisov <quopiks@gmail.com>
2020-08-27 11:28:34 +0300

    created validation functions

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import s from './MyPosts.module.scss';
 import Post from './Post/Post';
 import {Field, reduxForm} from 'redux-form';
+import {required, maxLengthCreator} from '../../../utils/validators/validators';
+
+const maxLength10 = maxLengthCreator(10);
 
 const MyPosts = (props) => {
     let postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount} key={p.id} />);
@@ -23,7 +26,7 @@ let AddNewPostForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field name='newPostText' component='textarea' />
+                <Field name='newPostText' component='textarea' validate={[required, maxLength10]} />
             </div>
             <button>Add post</button>
         </form>
