75e5219338d7d1632d477fc73008cb61758161c2
Aleksandr Sarkisov <quopiks@gmail.com>
2020-09-08 12:52:39 +0300

    added default photo profile

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.scss';
 import Preloader from '../../common/Preloader/Preloader';
+import userPhoto from '../../../assets/images/user.png';
 
 import ProfileStatus from './ProfileStatus';
 
@@ -11,7 +12,7 @@ const ProfileInfo = ({profile, status, updateStatus}) => {
     return (
         <div>
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large} alt='' />
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto} alt='' />
                 <h2>{profile.fullName}</h2>
                 <p className={s.description}>{profile.lookingForAJob ? profile.lookingForAJobDescription : 'Не ищу работу'}</p>
                 <ProfileStatus status={status} updateStatus={updateStatus} />
