1c32ce0d07e2ec7d0d51991f2dcc78898d504ce1
Aleksandr Sarkisov <quopiks@gmail.com>
2020-09-08 12:53:12 +0300

    remove useless import

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Preloader from './components/common/Preloader/Preloader';
 import {BrowserRouter} from 'react-router-dom';
 import store from './redux/redux-store';
 import {Provider} from 'react-redux';
-import {Suspense} from 'react';
 import {withSuspense} from './hoc/withSuspense';
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
